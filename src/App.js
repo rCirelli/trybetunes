71fc865802c87b481755797b59fa39fb1866587b
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-05-18 15:42:22 -0300

    1º requisito concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Album from './pages/Album';
+import ProfileEdit from './pages/ProfileEdit';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main className="bg-slate-900 text-slate-100 w-screen h-screen text-center">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/search" component={ Search } />
+            <Route exact path="/" component={ Login } />
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </main>
+    );
   }
 }
 
